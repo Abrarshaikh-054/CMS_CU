74c1f9e406c7a121cf5547301f58fc853a9db9e5
Abrar Shaikh <sheikhabrar054@gmail.com>
2025-08-20 19:11:44 +0530

    Update settings.py

--- CMS/CMS/settings.py
+++ CMS/CMS/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
     ('images', BASE_DIR.parent / 'images'),
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
